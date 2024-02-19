c6d9ad3de582ca9def93e1fbc9e4a47abaae7e87
sagittaurius <44752104+sagittaurius@users.noreply.github.com>
2024-02-19 22:36:09 +0700

    Update adblock_filter_compiler.py

--- adblock_filter_compiler.py
+++ adblock_filter_compiler.py
@@ -12,7 +12,7 @@ def is_valid_domain_name(domain: str) -> bool:
     return bool(domain_regex.match(domain))
 
 
-def filter_content_by_allowlist(filter_content: List[str], allowlist_domains: Set[str]) -> List[str]:
+def filter_content_by_allowlist_domains(filter_content: List[str], allowlist_domains: Set[str]) -> List[str]:
     """Removes allowed domains from the filter_content."""
     filtered_content = [
         '\n'.join(set(parse_filter_content(content)) - allowlist_domains)
@@ -23,33 +23,14 @@ def filter_content_by_allowlist(filter_content: List[str], allowlist_domains: Se
 
 def parse_filter_content(content: str) -> Set[str]:
     """Parses a filter content into AdBlock rules."""
-    adblock_rules = set()
-
-    for line in content.split('\n'):
-        line = line.strip()
-
-        # Ignore comments and empty lines
-        if not line or line[0] in ('#', '!'):
-            continue
-
-        # Ignore lines starting with '||www.'
-        if line.startswith('||www.'):
-            continue
-
-        # Check if line follows AdBlock syntax, else create new rule
-        if line.startswith('||') and line.endswith('^'):
-            adblock_rules.add(line)
-        else:
-            parts = line.split()
-            domain = parts[-1]
-            if is_valid_domain_name(domain):
-                adblock_rules.add(f'||{domain}^')
-
-    return adblock_rules
+    return {
+        line
+        for line in content.split('\n')
+        if line.strip() and line[0] not in ('#', '!') and not line.startswith('||www.')
+    }
 
 
-
-def generate_combined_filter(filter_content: List[str]) -> Tuple[str, int, int, int]:
+def generate_combined_filter_content(filter_content: List[str]) -> Tuple[str, int, int, int]:
     """Generates combined filter content by eliminating duplicates and redundant rules."""
     adblock_rules_set = set()
     base_domain_set = set()
@@ -60,7 +41,7 @@ def generate_combined_filter(filter_content: List[str]) -> Tuple[str, int, int, 
         adblock_rules = parse_filter_content(content)
         for rule in adblock_rules:
             domain = rule[2:-1]  # Remove '||' and '^'
-            base_domain = '.'.join(domain.split('.')[-3])  # Get the base domain (last two parts)
+            base_domain = '.'.join(domain.split('.')[-3:])  # Get the base domain (last three parts)
             if rule not in adblock_rules_set and base_domain not in base_domain_set:
                 adblock_rules_set.add(rule)
                 base_domain_set.add(base_domain)
@@ -88,13 +69,13 @@ def generate_filter_header(domain_count: int, duplicates_removed: int, redundant
 #=================================================================="""
 
 
-def process_filter_content(filter_content: List[str], allowlist_domains: List[str]) -> List[str]:
+def process_filter_content_with_allowlist_domains(filter_content: List[str], allowlist_domains: List[str]) -> List[str]:
     """Processes the allowed domains before filtering the content."""
-    filtered_content = filter_content_by_allowlist(filter_content, set(allowlist_domains))
+    filtered_content = filter_content_by_allowlist_domains(filter_content, set(allowlist_domains))
     return filtered_content
 
 
-def generate_blocklist():
+def generate_combined_filter_file():
     """Main function to fetch blocklists and generate a combined filter."""
     blocklist_urls = [
         "https://hostfiles.frogeye.fr/firstparty-only-trackers.txt",
@@ -122,8 +103,8 @@ def generate_blocklist():
     filter_content = [requests.get(url).text for url in blocklist_urls]
     allowlist_domains = requests.get(allowlist_urls[0]).text.split('\n')
 
-    filtered_content = process_filter_content(filter_content, allowlist_domains)
-    filtered_content, _, _, = generate_combined_filter(filtered_content)
+    filtered_content = process_filter_content_with_allowlist_domains(filter_content, allowlist_domains)
+    filtered_content, _, _, = generate_combined_filter_content(filtered_content)
 
     # Write the filter content to a file
     with open('blocklist.txt', 'w') as f:
@@ -131,4 +112,4 @@ def generate_blocklist():
 
 
 if __name__ == "__main__":
-    generate_blocklist()
+    generate_combined_filter_file()
