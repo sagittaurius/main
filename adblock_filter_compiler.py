2a075ac8d1bd0ae0be8211550213132ae6d86e32
sagittaurius <44752104+sagittaurius@users.noreply.github.com>
2024-02-22 02:19:46 +0700

    Update adblock_filter_compiler.py

--- adblock_filter_compiler.py
+++ adblock_filter_compiler.py
@@ -48,7 +48,7 @@ def generate_combined_filter_content(filter_content: List[str]) -> Tuple[str, in
         adblock_rules = parse_filter_content(content)
         for rule in adblock_rules:
             domain = rule[2:-1]  # Remove '||' and '^'
-            base_domain = '.'.join(domain.split('.')[-3:])  # Get the base domain (last three parts)
+            base_domain = '.'.join(domain.split('.')[-2:])  # Get the base domain (last three parts)
             if rule not in adblock_rules_set and base_domain not in base_domain_set:
                 adblock_rules_set.add(rule)
                 base_domain_set.add(base_domain)
